fae5a26f09cf292b443be2f8d7e248af525ba24c
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2026-09-09 22:03:20 +0430

    i add new blade code

--- resources/views/frontend/pages/about.blade.php
+++ resources/views/frontend/pages/about.blade.php
@@ -1,78 +1,10 @@
 @extends('frontend.dashboard')
 @section('frontend')
 
-        <!-- Page Title Banner Start -->
-        <section class="page_title_banner">
-            <div class="container">
-                <div class="content">
-                    <div class="title">
-                        <h1>About Us</h1>
-                        <img src="{{ asset('frontend/assets/media/shapes/tag-2.png') }}" alt="" class="tag">
-                    </div>
-                    <div class="educate-tilt"
-                        data-tilt-options='{ "glare": false, "maxGlare": 0, "maxTilt": 2, "speed": 700, "scale": 1 }'>
-                        <img src="{{ asset('frontend/assets/media/resources/page_title.png') }}" alt="About Khedmat" class="main_img">
-                    </div>
-                    <img src="{{ asset('frontend/assets/media/shapes/circle-lines-2.png') }}" alt="" class="circle_vector">
-                </div>
-            </div>
-        </section>
-        <!-- Page Title Banner End -->
+        @include('frontend.pages.partials.page-banner', ['title' => 'About Us'])
 
-        <!-- About Area Start -->
-        <section class="py-60">
-            <div class="container">
-                <div class="row align-items-center">
-                    <div class="col-lg-6 mb-48 mb-lg-0">
-                        <div class="heading mb-16">
-                            <h6 class="color-primary mb-8">–––– About Us</h6>
-                            <h2>Cultivating a Digital Learning <span class="fm-sec">Ecosystem.</span></h2>
-                        </div>
-                        <p class="mb-32">Lorem ipsum dolor sit amet consectetur. Non convallis sed id aliquam tempus.
-                            Volutpat tortor tincidunt egestas sit risus donec.</p>
-                        <div class="about__card mb-24 wow fadeInLeft" data-wow-delay="200ms">
-                            <div class="about__card__icon">
-                                <img src="{{ asset('frontend/assets/media/icons/Clock.png') }}" alt="Latest courses">
-                            </div>
-                            <div class="about__card__content">
-                                <h5 class="mb-4p">Latest Courses</h5>
-                                <p>Lorem ipsum dolor sit amet consectetur. Non convallis sed id aliquam tempus. Volutpat
-                                    tortor tincidunt egestas sit risus.</p>
-                            </div>
-                        </div>
-                        <div class="about__card mb-24 wow fadeInLeft" data-wow-delay="400ms">
-                            <div class="about__card__icon">
-                                <img src="{{ asset('frontend/assets/media/icons/WiFi.png') }}" alt="Live classes">
-                            </div>
-                            <div class="about__card__content">
-                                <h5 class="mb-4p">Live Classes</h5>
-                                <p>Lorem ipsum dolor sit amet consectetur. Non convallis sed id aliquam tempus. Volutpat
-                                    tortor tincidunt egestas sit risus.</p>
-                            </div>
-                        </div>
-                        <div class="about__card mb-24 wow fadeInLeft" data-wow-delay="600ms">
-                            <div class="about__card__icon">
-                                <img src="{{ asset('frontend/assets/media/icons/star.png') }}" alt="Nurtured students">
-                            </div>
-                            <div class="about__card__content">
-                                <h5 class="mb-4p">Nurtured Students</h5>
-                                <p>Lorem ipsum dolor sit amet consectetur. Non convallis sed id aliquam tempus. Volutpat
-                                    tortor tincidunt egestas sit risus.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-6">
-                        <div class="educate-tilt"
-                            data-tilt-options='{ "glare": false, "maxGlare": 0, "maxTilt": 2, "speed": 700, "scale": 1 }'>
-                            <img src="{{ asset('frontend/assets/media/resources/about-1.png') }}" alt="About Khedmat Academy">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <!-- About Area End -->
+        @include('frontend.section.About')
 
-        <!-- Benefits Area Start -->
         <section class="benefits py-60">
             <div class="container">
                 <div class="heading_block">
@@ -80,78 +12,12 @@
                     <h2 class="mb-32">Cultivating a Digital Learning <br><span class="fm-sec">Ecosystem.</span></h2>
                     <a href="{{ url('/') }}#courses" class="educate-btn"><span class="educate-btn__curve"></span>View All</a>
                 </div>
-                <div class="row">
-                    <div class="col-xxl-2 col-lg-3 col-sm-6 offset-xxl-2 card_block wow fadeInUp"
-                        data-wow-delay="200ms">
-                        <div class="feature__card mb-24 mb-xl-0">
-                            <div class="feature__icon">
-                                <img src="{{ asset('frontend/assets/media/icons/Pricing.png') }}" alt="Free trials">
-                            </div>
-                            <div class="feature__content">
-                                <h5 class="mb-8">Free Trials</h5>
-                                <p>Lorem ipsum dolor sit amet consectetu. </p>
-                                <img src="{{ asset('frontend/assets/media/shapes/feture-bg-shape.png') }}" alt="" class="feature-bg-shape">
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xxl-2 col-lg-3 col-sm-6 card_block wow fadeInUp" data-wow-delay="400ms">
-                        <div class="feature__card mb-24 mb-xl-0">
-                            <div class="feature__icon">
-                                <img src="{{ asset('frontend/assets/media/icons/Quality.png') }}" alt="Lifetime access">
-                            </div>
-                            <div class="feature__content">
-                                <h5 class="mb-8">Lifetime Access</h5>
-                                <p>Lorem ipsum dolor sit amet consectetu. </p>
-                                <img src="{{ asset('frontend/assets/media/shapes/feture-bg-shape.png') }}" alt="" class="feature-bg-shape">
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xxl-2 col-lg-3 col-sm-6 card_block wow fadeInUp" data-wow-delay="600ms">
-                        <div class="feature__card mb-24 mb-sm-0">
-                            <div class="feature__icon">
-                                <img src="{{ asset('frontend/assets/media/icons/Check-mark.png') }}" alt="Best teachers">
-                            </div>
-                            <div class="feature__content">
-                                <h5 class="mb-8">Best Teachers</h5>
-                                <p>Lorem ipsum dolor sit amet consectetu. </p>
-                                <img src="{{ asset('frontend/assets/media/shapes/feture-bg-shape.png') }}" alt="" class="feature-bg-shape">
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xxl-2 col-lg-3 col-sm-6 card_block wow fadeInUp" data-wow-delay="800ms">
-                        <div class="feature__card">
-                            <div class="feature__icon">
-                                <img src="{{ asset('frontend/assets/media/icons/Support.png') }}" alt="24/7 support">
-                            </div>
-                            <div class="feature__content">
-                                <h5 class="mb-8">24/7 Support</h5>
-                                <p>Lorem ipsum dolor sit amet consectetu. </p>
-                                <img src="{{ asset('frontend/assets/media/shapes/feture-bg-shape.png') }}" alt="" class="feature-bg-shape">
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xxl-2 col-sm-6 card_block wow fadeInUp d-xxl-block d-none" data-wow-delay="800ms">
-                        <div class="feature__card">
-                            <div class="feature__icon">
-                                <img src="{{ asset('frontend/assets/media/icons/Transfer.png') }}" alt="Daily updates">
-                            </div>
-                            <div class="feature__content">
-                                <h5 class="mb-8">Daily Updates</h5>
-                                <p>Lorem ipsum dolor sit amet consectetu. </p>
-                                <img src="{{ asset('frontend/assets/media/shapes/feture-bg-shape.png') }}" alt="" class="feature-bg-shape">
-                            </div>
-                        </div>
-                    </div>
-                </div>
             </div>
         </section>
-        <!-- Benefits Area End -->
+        @include('frontend.section.Features')
 
-        <!-- Team Area Start -->
         @include('frontend.section.Team')
-        <!-- Team Area End -->
 
-        <!-- Stats Area Start -->
         <section class="py-60">
             <div class="container">
                 <div class="row">
@@ -176,7 +42,7 @@
                                     <span>k</span>
                                 </h2>
                             </div>
-                            <h5>Students Enrolled</h5>
+                            <h5>Online Courses</h5>
                         </div>
                     </div>
                     <div class="col-lg-3">
@@ -188,7 +54,7 @@
                                     <span>%</span>
                                 </h2>
                             </div>
-                            <h5>Students Enrolled</h5>
+                            <h5>Success Rate</h5>
                         </div>
                     </div>
                     <div class="col-lg-3">
@@ -200,20 +66,14 @@
                                     <span>+</span>
                                 </h2>
                             </div>
-                            <h5>Students Enrolled</h5>
+                            <h5>Expert Teachers</h5>
                         </div>
                     </div>
                 </div>
             </div>
         </section>
-        <!-- Stats Area End -->
 
-        <!-- Testimonials Area Start -->
         @include('frontend.section.Testimonials')
-        <!-- Testimonials Area End -->
-
-        <!-- Discount Banner Area Start -->
         @include('frontend.section.Discount')
-        <!-- Discount Banner Area End -->
 
 @endsection
--- resources/views/frontend/pages/join.blade.php
+++ resources/views/frontend/pages/join.blade.php
@@ -1,94 +1,65 @@
 @extends('frontend.dashboard')
 @section('frontend')
 
-<!-- Page Title Banner Start -->
-        <section class="page_title_banner">
-            <div class="container">
-                <div class="content">
-                    <div class="title">
-                        <h1>Become A Teacher</h1>
-                        <img src="{{ asset('frontend/assets/media/shapes/tag-2.png') }}" alt="" class="tag">
-                    </div>
-                    <div class="educate-tilt"
-                        data-tilt-options='{ "glare": false, "maxGlare": 0, "maxTilt": 2, "speed": 700, "scale": 1 }'>
-                        <img src="{{ asset('frontend/assets/media/resources/page_title.png') }}" alt="" class="main_img">
-                    </div>
-                    <img src="{{ asset('frontend/assets/media/shapes/circle-lines-2.png') }}" alt="" class="circle_vector">
-                </div>
-            </div>
-        </section>
-        <!-- Page Title Banner End -->
+        @include('frontend.pages.partials.page-banner', ['title' => 'Become A Teacher'])
 
-        <!-- join Team Area Start -->
         <section class="join_team py-60">
             <div class="container">
                 <div class="row">
                     <div class="col-lg-7">
-                        <h6 class="color-primary mb-8">–––– Apply Info</h6>
-                        <h2 class="mb-16">Want to Become A Teacher?<br> Drop Us <span class="fm-sec">Your CV!</span>
-                        </h2>
-                        <p class="mb-32">Lorem ipsum dolor sit amet consectetur. Non convallis sed id <br>aliquam
-                            tempus. Volutpat
-                            tortor tincidunt egestas sit risus donec.</p>
+                        <h6 class="color-primary mb-8">–––– {{ $join?->subtitle ?? 'Apply Info' }}</h6>
+                        <h2 class="mb-16">{!! $join?->title ?? 'Want to Become A Teacher?<br> Drop Us <span class="fm-sec">Your CV!</span>' !!}</h2>
+                        <p class="mb-32">{{ $join?->description ?? 'Share your details and we will review your application.' }}</p>
                         <div class="block mb-16">
-                            <h5 class="mb-8">Rules & Regulations</h5>
-                            <p class="mb-16">Lorem ipsum dolor sit amet consectetur. Non convallis sed id aliquam
-                                tempus. Volutpat tortor tincidunt egestas sit risus donec. Non convallis sed id aliquam
-                                tempus. </p>
+                            <h5 class="mb-8">{{ $join?->rules_title ?? 'Rules & Regulations' }}</h5>
+                            <p class="mb-16">{{ $join?->rules_text ?? 'Please follow the academy teaching guidelines.' }}</p>
                             <ul class="unstyled rules">
+                                @forelse (($join?->ruleList() ?? []) as $rule)
                                 <li class="mb-16"><i class="fal fa-check"></i>
-                                    <p>Lorem ipsum dolor sit amet consectetur.</p>
+                                    <p>{{ $rule }}</p>
                                 </li>
+                                @empty
                                 <li class="mb-16"><i class="fal fa-check"></i>
-                                    <p>Lorem ipsum dolor sit amet consectetur.</p>
+                                    <p>Submit a complete CV and contact details.</p>
                                 </li>
-                                <li class="mb-16"><i class="fal fa-check"></i>
-                                    <p>Lorem ipsum dolor sit amet consectetur.</p>
-                                </li>
+                                @endforelse
                             </ul>
                         </div>
                         <div class="block">
-                            <h5 class="mb-16">Qualification Requirement</h5>
-                            <p class="mb-16">Lorem ipsum dolor sit amet consectetur. Non convallis sed id aliquam
-                                tempus. Volutpat tortor tincidunt egestas sit risus donec. Lorem ipsum dolor sit amet
-                                consectetur. Non convallis sed id aliquam tempus. Volutpat tortor tincidunt egestas sit
-                                risus donec.Lorem ipsum dolor sit amet consectetur. Non convallis sed id aliquam tempus.
-                                Volutpat tortor tincidunt egestas sit risus donec.Lorem ipsum dolor sit amet
-                                consectetur. Non convallis sed id aliquam tempus. Volutpat tortor tincidunt egestas sit
-                                risus donec.</p>
+                            <h5 class="mb-16">{{ $join?->qualification_title ?? 'Qualification Requirement' }}</h5>
+                            <p class="mb-16">{{ $join?->qualification_text ?? 'Teaching experience and subject knowledge are required.' }}</p>
                         </div>
                     </div>
                     <div class="col-lg-5">
                         <div class="join_form_block">
                             <h4 class="mb-24">Apply Form</h4>
-                            <form action="#" class="form-validator">
+                            <form action="{{ route('join.apply') }}" method="POST" enctype="multipart/form-data" class="form-validator">
+                                @csrf
                                 <div class="mb-24">
-                                    <input type="text" class="form-control" name="first-name" required
+                                    <input type="text" class="form-control" name="first_name" required
                                         placeholder="First Name">
                                 </div>
                                 <div class="mb-24">
-                                    <input type="text" class="form-control" name="last-name" required
+                                    <input type="text" class="form-control" name="last_name" required
                                         placeholder="Last Name">
                                 </div>
                                 <div class="mb-24">
                                     <input type="email" class="form-control" name="email" required placeholder="Email">
                                 </div>
                                 <div class="mb-24">
-                                    <input type="tel" class="form-control" name="tel" required
+                                    <input type="tel" class="form-control" name="phone" required
                                         placeholder="Phone Number">
                                 </div>
                                 <div class="mb-24">
-                                    <input type="file" class="form-control" name="cv" required>
+                                    <input type="file" class="form-control" name="cv" accept=".pdf,.doc,.docx">
                                 </div>
                                 <button type="submit" class="educate-btn"><span class="educate-btn__curve"></span>Apply
                                     Now</button>
                             </form>
                         </div>
                     </div>
-
                 </div>
             </div>
         </section>
-        <!-- join Team Area End -->
 
 @endsection
