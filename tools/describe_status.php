71c65ba89101b8150f09549fb53cdc521e05e51c
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2026-09-12 22:11:11 +0430

    i add the new code

---
+++ tools/describe_status.php
@@ -0,0 +1,7 @@
+<?php
+
+require __DIR__.'/../vendor/autoload.php';
+$app = require __DIR__.'/../bootstrap/app.php';
+$app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
+
+print_r(Illuminate\Support\Facades\DB::select('SHOW COLUMNS FROM users LIKE ?', ['status']));
